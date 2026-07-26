d9112c4946aecac3f8968c6b059be9b35da0eaff
John Axel De Leon <224013364+JohnAxel04@users.noreply.github.com>
2026-07-26 21:51:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//Image
+const mainImg = document.getElementById("mainImg");
+//Info
+const mainName = document.getElementById("mainName");
+const mainId = document.getElementById("mainId");
+const mainType = document.getElementById("mainType");
+const mainAbility = document.getElementById("mainAbility");
+const mainHeight = document.getElementById("mainHeight");
+const mainWeight = document.getElementById("mainWeight");
+//Stats
+const mainHp = document.getElementById("mainHp");
+const mainAttack = document.getElementById("mainAttack");
+const mainDef = document.getElementById("mainDef");
+const mainSpatk = document.getElementById("mainSpatk");
+const mainSpdef = document.getElementById("mainSpdef");
+const mainSpeed = document.getElementById("mainSpeed");
